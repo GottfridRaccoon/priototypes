b23c09809c0c3e55e8ba0cb83bf239e191884edc
GottfridEricsson <westraccoon@yandex.ru>
2019-08-01 17:21:08 +0300

    this is prototype

---
+++ objective.js
@@ -0,0 +1,69 @@
+var txt = '"name" in Math'
+var d = document
+
+  function Hi() {
+    d.write (txt+"->"+eval(txt)+"<br>")
+  }
+Hi()
+    var A = {color:"red"}
+    var B = new Object ()
+      B.number = 10
+
+      showAll()
+      Object.prototype.name = "Object A"
+Hi()
+      B.name ="Object B"
+    document.write ("MATH NAME IS-> "+Math.name+"<br>")
+showAll()
+      delete Object.prototype.name
+Hi()
+showAll()
+
+    function show (obj){
+      for (s in obj)
+      {document.write (s+"|"+obj[s]+"|")
+     }
+
+d.write("<br>")
+
+}
+    function showAll(){
+      d.write("Объект А: ")
+      show(A)
+      d.write ("Объект Б: ")
+      show (B)
+      d.write("<br>")
+}
+d.write("<br>")
+d.write("<br>")
+d.write("<br>")
+    //тут уже будет мой код на основе предыдущего :
+document.write ("<b>Начало моего кода</b>"+"<br>")
+    let Objhuman= {name:"Alexander"}
+    let Objhuman2 = new Object()
+      Objhuman2.name= "Dmitriy"
+      is()
+    Object.prototype.years = 33
+    is()
+          delete Object.prototype.years
+        is()
+          Objhuman2.years = 22
+          is()
+
+      function sl(thename) {
+        for (s in thename){
+          document.write(s+"->"+thename[s]+' | ')
+          //проверка наличия свойств и методов в объекте
+        }
+        document.write("<br>")
+      }
+function is() {
+  sl(Objhuman)
+  sl(Objhuman2)
+document.write("<br>")
+}
+document.write ("<br>")
+document.write("<hr><b>ТАК СЛОЖНО</b></hr>")
+function functionName(na) {
+
+}
